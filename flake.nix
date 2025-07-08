e7aeaa58af122183dcd7720cdb2528fd8b65cf58
Aleksey Sidorov <sauron1987@gmail.com>
2025-07-08 19:57:57 +0300

    Оптимизация сборки для уменьшения использования места на диске

--- flake.nix
+++ flake.nix
@@ -6,6 +6,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  # Конфигурация Nix для этого flake
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.ztier.in" # cache from hydra-riscv64 project
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.ztier.link-1:3P5j2ZB9dNgFFFVkCQWT3mh0E+S3rIWtZvoql64UaXM="
+    ];
+    keep-outputs = true;
+    keep-derivations = true;
+  };
+
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "riscv64-linux" ] (system:
       let
@@ -18,6 +32,8 @@
           crossSystem = crossSystem;
           config = {
             allowUnfree = true;
+            # Оптимизации для сборки
+            permittedInsecurePackages = [ "openssl-1.1.1w" ];
           };
         };
 
@@ -25,11 +41,25 @@
       in
       {
         packages = {
-          installer = (pkgs.nixos {
-            modules = [
+          installer = (pkgs.nixos ({
+            imports = [
               ./sd-image-orangepi-rv2-installer.nix
             ];
-          }).config.system.build.sdImage;
+
+            # Конфигурации специфичные для сборки
+            nixpkgs.config.allowUnfree = true;
+            nix.settings = {
+              max-jobs = "auto";
+              cores = 0;
+              sandbox = true;
+              extra-sandbox-paths = [ "/bin/sh=/bin/sh" ];
+              substituters = [ "https://cache.nixos.org" "https://cache.ztier.in" ];
+              trusted-public-keys = [
+                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                "cache.ztier.link-1:3P5j2ZB9dNgFFFVkCQWT3mh0E+S3rIWtZvoql64UaXM="
+              ];
+            };
+          })).config.system.build.sdImage;
 
           default = self.packages.${system}.installer;
         };
--- linux-orangepi-ky.nix
+++ linux-orangepi-ky.nix
@@ -11,68 +11,100 @@
 let
   modDirVersion = "6.6.63";
 in
-(buildLinux (args // {
-  inherit modDirVersion;
-  version = "${modDirVersion}-ky";
+(buildLinux
+  (args // {
+    inherit modDirVersion;
+    version = "${modDirVersion}-ky";
 
-  src = fetchFromGitHub {
-    owner = "orangepi-xunlong";
-    repo = "linux-orangepi";
-    rev = "ae9e974d3e19f460b6397bfe8f0f1417a073ce05";
-    hash = "sha256-3mJMruK/oQNsw9VWDVln1CZ+Zv8MXQBy4gmsi7EbX+s=";
-  };
+    src = fetchFromGitHub {
+      owner = "orangepi-xunlong";
+      repo = "linux-orangepi";
+      rev = "ae9e974d3e19f460b6397bfe8f0f1417a073ce05";
+      hash = "sha256-3mJMruK/oQNsw9VWDVln1CZ+Zv8MXQBy4gmsi7EbX+s=";
+    };
 
-  defconfig = "x1_defconfig";
+    defconfig = "x1_defconfig";
 
-  structuredExtraConfig = with lib.kernel; {
-    # ttyS0
-    SERIAL_8250 = lib.mkForce no;
-    SERIAL_PXA_KY_X1 = yes;
+    structuredExtraConfig = with lib.kernel; {
+      # ttyS0
+      SERIAL_8250 = lib.mkForce no;
+      SERIAL_PXA_KY_X1 = yes;
 
-    # Broken
-    SPI_DESIGNWARE_EXT = no;
-    TOUCHSCREEN_GT9XX = no;
-    TOUCHSCREEN_GOODIX = no;
-    TYPEC_HUSB239 = no;
-    USB_NET_ASIX = no;
-    WLAN_VENDOR_REALTEK = no;
+      # Broken
+      SPI_DESIGNWARE_EXT = no;
+      TOUCHSCREEN_GT9XX = no;
+      TOUCHSCREEN_GOODIX = no;
+      TYPEC_HUSB239 = no;
+      USB_NET_ASIX = no;
+      WLAN_VENDOR_REALTEK = no;
 
-    ETHERCAT = no;
-    TOUCHSCREEN_FTS = no;
+      ETHERCAT = no;
+      TOUCHSCREEN_FTS = no;
 
-    # Make the build faster
-    COMPILE_TEST = no;
-    DRM_NOUVEAU = no;
-    DRM_AMDGPU = no;
-  };
+      # Make the build faster and smaller
+      COMPILE_TEST = no;
+      DRM_NOUVEAU = no;
+      DRM_AMDGPU = no;
 
-  kernelPatches = [
-    {
-      patch = fetchpatch {
-        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/f52ff2db8a163c51c770fe72af6a6e5c8f315617/linux-01-enable_pxa_pwm_on_ky_x1.patch";
-        hash = "sha256-nDTiRfAhgok06tP//m9NG2PoEfpKQS2Gz5mpHSHo9Pg=";
-      };
-    }
-    {
-      patch = fetchpatch {
-        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/f52ff2db8a163c51c770fe72af6a6e5c8f315617/linux-02-add_DMA_BUF_ns_import_to_amvx.patch";
-        hash = "sha256-V2vWHTmai4PZapTGlAJnJUetv+P4C0xK01GgFUdcCKw=";
-      };
-    }
-    {
-      patch = ./bcmdhd-makefile-include-fix.patch;
-    }
-  ];
+      # Reduce kernel size significantly
+      DEBUG_INFO = no;
+      DEBUG_KERNEL = no;
+      DEBUG_MISC = no;
 
-  extraMeta = {
-    branch = lib.versions.major modDirVersion;
-    platforms = [ "riscv64-linux" ];
-  };
-} // (args.argsOverride or { }))).overrideAttrs (old: {
+      # Disable unneeded graphics drivers
+      DRM_RADEON = no;
+      DRM_I915 = no;
+      DRM_VMWGFX = no;
+      SOUND_OSS = no;
+
+      # Disable virtualization features to save space
+      KVM = no;
+      VIRTUALIZATION = no;
+
+      # Disable unneeded network protocols
+      IPX = no;
+      ATALK = no;
+
+      # Disable HID devices that aren't needed
+      HID_APPLE = no;
+      HID_LOGITECH = no;
+      HID_MICROSOFT = no;
+    };
+
+    kernelPatches = [
+      {
+        patch = fetchpatch {
+          url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/f52ff2db8a163c51c770fe72af6a6e5c8f315617/linux-01-enable_pxa_pwm_on_ky_x1.patch";
+          hash = "sha256-nDTiRfAhgok06tP//m9NG2PoEfpKQS2Gz5mpHSHo9Pg=";
+        };
+      }
+      {
+        patch = fetchpatch {
+          url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/f52ff2db8a163c51c770fe72af6a6e5c8f315617/linux-02-add_DMA_BUF_ns_import_to_amvx.patch";
+          hash = "sha256-V2vWHTmai4PZapTGlAJnJUetv+P4C0xK01GgFUdcCKw=";
+        };
+      }
+      {
+        patch = ./bcmdhd-makefile-include-fix.patch;
+      }
+    ];
+
+    extraMeta = {
+      branch = lib.versions.major modDirVersion;
+      platforms = [ "riscv64-linux" ];
+    };
+  } // (args.argsOverride or { }))).overrideAttrs (old: {
   nativeBuildInputs = old.nativeBuildInputs or [ ] ++ (with buildPackages; [ ubootTools ]);
   env.NIX_CFLAGS_COMPILE = toString [
     "-Wno-error=incompatible-pointer-types"
     "-Wno-error=implicit-function-declaration"
     "-Wno-error=int-conversion"
   ];
+
+  # Оптимизируем использование места
+  enableParallelBuilding = true;
+  NIX_ENFORCE_NO_NATIVE = false;
+
+  # Уменьшаем отладочную информацию
+  dontStrip = false;
 })
