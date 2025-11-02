3143b65cff308683980c5d895a06066b821c5823
Aleksey Sidorov <sauron1987@gmail.com>
2025-11-02 16:31:32 +0300

    Bump orangepi-ky kernel to 6.6.75
    Add sequential kernel patch archives for the intermediate releases and
    reflow kernelPatches entries to single-line records

--- pkgs/linux/linux-orangepi-ky.nix
+++ pkgs/linux/linux-orangepi-ky.nix
@@ -10,7 +10,7 @@
 } @ args:
 
 let
-  modDirVersion = "6.6.64";
+  modDirVersion = "6.6.75";
 
   fetchCwtPatch = { name, hash }: {
     inherit name;
@@ -77,30 +77,23 @@ in
   };
 
   kernelPatches = [
-    {
-      name = "bcmdhd-makefile-include-fix";
-      patch = ./bcmdhd-makefile-include-fix.patch;
-    }
-    (fetchCwtPatch {
-      name = "linux-01-enable_pxa_pwm_on_ky_x1.patch";
-      hash = "sha256-nDTiRfAhgok06tP//m9NG2PoEfpKQS2Gz5mpHSHo9Pg=";
-    })
-    (fetchCwtPatch {
-      name = "linux-02-add_DMA_BUF_ns_import_to_amvx.patch";
-      hash = "sha256-V2vWHTmai4PZapTGlAJnJUetv+P4C0xK01GgFUdcCKw=";
-    })
-    (fetchCwtPatch {
-      name = "linux-03-enable-ky_x1-clocksource.patch";
-      hash = "sha256-2UpkCWH+QcQtJWOT1X7YFCiUDZcksyk/Wf/frHb3waU=";
-    })
-    (fetchCwtPatch {
-      name = "linux-04-fix-timer-ky_x1-conflict-types.patch";
-      hash = "sha256-wtUTGfisAzacPlN76lZTLgU8biTicTZMLtCDxgcLV40=";
-    })
-    (fetchKernelPatchArchive {
-      name = "patch-6.6.63-64.xz";
-      hash = "sha256-9H7xASKq8vpPPh8FtL7BmtaYp5BpLbmTDQv01CERZNA=";
-    })
+    { name = "bcmdhd-makefile-include-fix"; patch = ./bcmdhd-makefile-include-fix.patch; }
+    (fetchCwtPatch { name = "linux-01-enable_pxa_pwm_on_ky_x1.patch"; hash = "sha256-nDTiRfAhgok06tP//m9NG2PoEfpKQS2Gz5mpHSHo9Pg="; })
+    (fetchCwtPatch { name = "linux-02-add_DMA_BUF_ns_import_to_amvx.patch"; hash = "sha256-V2vWHTmai4PZapTGlAJnJUetv+P4C0xK01GgFUdcCKw="; })
+    (fetchCwtPatch { name = "linux-03-enable-ky_x1-clocksource.patch"; hash = "sha256-2UpkCWH+QcQtJWOT1X7YFCiUDZcksyk/Wf/frHb3waU="; })
+    (fetchCwtPatch { name = "linux-04-fix-timer-ky_x1-conflict-types.patch"; hash = "sha256-wtUTGfisAzacPlN76lZTLgU8biTicTZMLtCDxgcLV40="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.63-64.xz"; hash = "sha256-9H7xASKq8vpPPh8FtL7BmtaYp5BpLbmTDQv01CERZNA="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.64-65.xz"; hash = "sha256-/b9VPAOIteUQ4jA5RqbP0mvNA7xiOJ+WdaipB5E2Ue4="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.65-66.xz"; hash = "sha256-TP282k+l0TxQbWfqxw1kvhQyRMlAEuBM/dQ0gCzhTec="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.66-67.xz"; hash = "sha256-TaJu/j5TfiKqXujeUnllefD6iwSb/n/BjtONdatqnno="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.67-68.xz"; hash = "sha256-GgpZNXWV1jLdj8h+0P79EUOUmW9qbHjRoGf/QopALtE="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.68-69.xz"; hash = "sha256-mj6pH5xUpuclNC3Fy0aV5CPXzSczoo+lgmDDsf9tL5M="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.69-70.xz"; hash = "sha256-fOXrgcCxbRlDA8k0s1s7VS670jIVEw1j+8Rle0ah5+w="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.70-71.xz"; hash = "sha256-CA6ft0Q9MQnDMye6tQ8TvgrnS1HpGOx1XTIGdeAmCC4="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.71-72.xz"; hash = "sha256-dF7BZ4ltcU6l6+3hMGzKehkLiEXE+zeBZTqMsvtRCmg="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.72-73.xz"; hash = "sha256-Dx5bXfDOPX/UCBOqCMYMYM/s4DH0nR/JxaIXlfYW0gw="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.73-74.xz"; hash = "sha256-b8zNPBFe3c+L7gBinhY71KzaNkKSnOL5pGJFimZHxPg="; })
+    (fetchKernelPatchArchive { name = "patch-6.6.74-75.xz"; hash = "sha256-viHS2I8Z9N2iUs0AIXUsF8EC+7z8QlbtvQKyUAoalN4="; })
   ];
 
   extraMeta = {
