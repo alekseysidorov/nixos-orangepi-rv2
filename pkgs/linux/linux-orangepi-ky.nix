05f4809bc8c375e16cea0a7b88891f27733aef70
Aleksey Sidorov <sauron1987@gmail.com>
2025-11-02 16:09:04 +0300

    Update Orangepi KY kernel patches and URLs
    Point existing patches to upstream "main" branch and replace the local
    bcmdhd makefile patch with two fetched patches: enable-ky_x1-clocksource
    and fix-timer-ky_x1-conflict-types

--- pkgs/linux/linux-orangepi-ky.nix
+++ pkgs/linux/linux-orangepi-ky.nix
@@ -52,20 +52,30 @@ in
     {
       name = "enable_pxa_pwm_on_ky_x1";
       patch = fetchpatch {
-        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/f52ff2db8a163c51c770fe72af6a6e5c8f315617/linux-01-enable_pxa_pwm_on_ky_x1.patch";
+        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/main/linux-01-enable_pxa_pwm_on_ky_x1.patch";
         hash = "sha256-nDTiRfAhgok06tP//m9NG2PoEfpKQS2Gz5mpHSHo9Pg=";
       };
     }
     {
       name = "add_DMA_BUF_ns_import_to_amvx";
       patch = fetchpatch {
-        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/f52ff2db8a163c51c770fe72af6a6e5c8f315617/linux-02-add_DMA_BUF_ns_import_to_amvx.patch";
+        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/main/linux-02-add_DMA_BUF_ns_import_to_amvx.patch";
         hash = "sha256-V2vWHTmai4PZapTGlAJnJUetv+P4C0xK01GgFUdcCKw=";
       };
     }
     {
-      name = "bcmdhd-makefile-include-fix";
-      patch = ./bcmdhd-makefile-include-fix.patch;
+      name = "enable-ky_x1-clocksource";
+      patch = fetchpatch {
+        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/main/linux-03-enable-ky_x1-clocksource.patch";
+        hash = "sha256-2UpkCWH+QcQtJWOT1X7YFCiUDZcksyk/Wf/frHb3waU=";
+      };
+    }
+    {
+      name = "fix-timer-ky_x1-conflict-types";
+      patch = fetchpatch {
+        url = "https://raw.githubusercontent.com/cwt-opi-rv2/linux-cwt-orangepi-ky/main/linux-04-fix-timer-ky_x1-conflict-types.patch";
+        hash = "sha256-wtUTGfisAzacPlN76lZTLgU8biTicTZMLtCDxgcLV40=";
+      };
     }
   ];
 
