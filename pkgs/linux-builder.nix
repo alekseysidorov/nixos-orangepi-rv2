5b87e5cf384287a6259c9e0b037afb3283290b3c
Aleksey Sidorov <sauron1987@gmail.com>
2026-09-26 15:04:07 +0300

    fix: preserve builder runner passthru

--- pkgs/linux-builder.nix
+++ pkgs/linux-builder.nix
@@ -27,6 +27,5 @@ in
 configuration.config.system.build.vm.overrideAttrs (old: {
   passthru = (old.passthru or { }) // {
     nixosConfiguration = configuration;
-    run-builder = configuration.config.system.build."run-builder";
   };
 })
