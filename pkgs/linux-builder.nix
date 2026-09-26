8eae99a9e7bd58d82b114b4a08fb90e578366994
Aleksey Sidorov <sauron1987@gmail.com>
2026-09-26 15:19:26 +0300

    fix: use supported RISC-V cross package set

--- pkgs/linux-builder.nix
+++ pkgs/linux-builder.nix
@@ -16,7 +16,7 @@ let
         # Use the cross package set as the guest package universe. Without
         # this, qemu-vm sees the evaluator's x86_64 package set and selects
         # qemu-system-x86_64 even though hostPlatform is RISC-V.
-        nixpkgs.pkgs = pkgs.pkgsCross.riscv64-unknown-linux-gnu;
+        nixpkgs.pkgs = pkgs.pkgsCross.riscv64;
         nixpkgs.buildPlatform = system;
         nixpkgs.hostPlatform = hostPlatform;
 
